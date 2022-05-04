498902b7f0343da2f22b5f8bf2d7914acd0d3a8f
unknown <ganitniduniyanikunj10@gmail.com>
2022-05-04 12:05:37 +0530

    Angular notes

--- Session-4/myapp/src/app/binding/binding.component.ts
+++ Session-4/myapp/src/app/binding/binding.component.ts
@@ -15,5 +15,13 @@ export class BindingComponent implements OnInit {
   name:string="Nikunj Soni";
   age:number=35;
   url:string="https://www.google.com";
+  we:number=700;
+
+  products=[{name:"pencil",price:"8.3",available:"05-05-2022",rating:4.8},
+            {name:"pen",price:"10",available:"10-05-2022",rating:4.6},
+            {name:"eraser",price:"5.5",available:"05-06-2022",rating:5},
+            {name:"scale",price:"20",available:"05-08-2022",rating:4},
+            {name:"strencil",price:"15",available:"05-09-2022",rating:3.5},
+          ]
 
 }
